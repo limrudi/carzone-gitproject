3ad8baa453d7ee6b82dbf2c61eb9b4608d509e17
limrudi <rudilim@gmail.com>
2022-06-28 20:28:45 +0800

    Added Allowed Hosts

--- carzone/settings.py
+++ carzone/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['radiant-plains-50049.herokuapp.com', 'childactually.com']
+ALLOWED_HOSTS = ['radiant-plains-50049.herokuapp.com', 'childactually.com', 'www.childactually.com']
 
 LOGIN_REDIRECT_URL = 'dashboard'
 
